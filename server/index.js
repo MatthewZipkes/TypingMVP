2e0872d41208964c99e3ce2ae8f2abc6b9353f69
Matthew Zipkes <matthewzipkes@gmail.com>
2021-12-02 23:32:29 -0600

    added API

--- server/index.js
+++ server/index.js
@@ -3,11 +3,18 @@ const app = express();
 const socketio = require('socket.io');
 const mongoose = require('mongoose');
 const Game = require('../schema/game');
+const getQuote = require('./quotes')
 const port = 3001;
 
 app.use(express.json())
 app.use(express.static(__dirname + '/../dist'))
 
+// app.get('/', (req, res) => {
+//   getQuote();
+//   // res.send(quote);
+// })
+
+getQuote();
 const expressServer = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
---
+++ server/quotes.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+const url = 'https://api.quotable.io/random?minLength=200&maxLength=400'
+
+const getQuote = ()=> {
+  return axios.get(url)
+  .then(response => {
+    response.data.content.split(' ')
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
+module.exports = getQuote;
