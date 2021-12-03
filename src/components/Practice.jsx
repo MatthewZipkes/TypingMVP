88954d9b47668e7dfcbd88e5d50a7cb5a06139b6
Matthew Zipkes <matthewzipkes@gmail.com>
2021-12-03 10:52:01 -0600

    working on practice first then worry about sockets

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -3,27 +3,35 @@ import axios from 'axios'
 
 function Practice() {
     useEffect( () => {
-    fetchQoutes();
+    fetchQuotes();
     },[])
-    
-    const [qoutes, setQoutes] = useState([])
-    const fetchQoutes = () => {
+
+    const [quotes, setQuotes] = useState([])
+    const fetchQuotes = () => {
         return (axios.get('https://api.quotable.io/random?minLength=200&maxLength=400')
         .then(response => {
-          let newQoute = response.data.content.split(' ')
-          console.log(newQoute)
-          setQoutes(newQoute)
+          let newQuote = response.data.content.split(' ')
+          console.log(newQuote)
+          setQuotes(newQuote)
         })
         .catch(error => {
           console.log(error)
         }))
     }
-    
+
 
     return (
-        <div>
-            {qoutes.join(' ')}
-        </div>
+        <>
+          {quotes.map(word => (
+            <div className='wordStyle'>
+              {word.split('').map(letter => {
+                return(
+                  <letter>{letter}</letter>
+                )
+              })}
+            </div>
+         ))}
+        </>
     )
 }
 
